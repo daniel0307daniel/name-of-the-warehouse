2743b2ddd741374292fd0e124ce6bc363e3f6555
daniel <409500112@gms.tku.edu.tw>
2021-09-02 10:14:32 +0800

    for lesson two add C_one.cpp use 'git pull'

---
+++ build/example/CMakeFiles/C_one.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_one.dir/src/C_one.cpp.o"
+  "/home/daniel/name-of-the-warehouse/devel/lib/example/C_one.pdb"
+  "/home/daniel/name-of-the-warehouse/devel/lib/example/C_one"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_one.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
