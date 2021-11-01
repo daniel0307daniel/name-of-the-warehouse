b2dd806c35acc7b75c9195fdcfbbb259cfc810ad
daniel <409500112@gms.tku.edu.tw>
2021-11-01 21:08:09 +0800

    PY lesson2 add PY

---
+++ src/example/src/PY.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+print("iclab_xiao_ming666")
+s=input("aa:")
+if s=="aa":
+	print("iclab_xiao_ming666")
+else:
+	print("no")
+from time import sleep
+n=1
+while n<11:
+	if n==10:
+		sleep(1)
+		print("time up!")
+		n+=1
+	elif n<10:
+		sleep(1)
+		print(n)
+		n+=1
+
